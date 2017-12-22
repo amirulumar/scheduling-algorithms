4e8d982c1975ff16e4971c807d83bf0524b0c5cc
ming2b1234 <ming2b@yahoo.com>
2017-12-22 22:04:38 +0800

    Add files via upload

--- SJFP/src/SJFP.java
+++ SJFP/src/SJFP.java
@@ -1,18 +1,29 @@
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import com.sun.management.OperatingSystemMXBean;
 import java.util.Scanner;
+import javax.management.MBeanServerConnection;
+
 public class SJFP {
- public static void main(String args[])
+ public static void main(String args[]) throws IOException
  {
       int n;
       Scanner sc=new Scanner(System.in);
+      //Cpu Usage code
+      MBeanServerConnection mbsc = ManagementFactory.getPlatformMBeanServer();
+      OperatingSystemMXBean osMBean = ManagementFactory.newPlatformMXBeanProxy(mbsc, ManagementFactory.OPERATING_SYSTEM_MXBEAN_NAME, OperatingSystemMXBean.class);
+      double nanoBefore = System.nanoTime();
+      double cpuBefore = osMBean.getProcessCpuTime();
       System.out.println("Please enter the number of Processes: ");
        n = sc.nextInt();
-       int process[][] = new int[n + 1][4];
+       int process[][] = new int[n + 1][5];
        for(int i = 1; i <= n; i++)
        {
       System.out.println("Please enter the Arrival Time for Process " + i + ": ");
       process[i][0] = sc.nextInt();
       System.out.println("Please enter the Burst Time for Process " + i + ": ");
       process[i][1] = sc.nextInt();
+      process[i][4] = process [i][1];
      }
        System.out.println();
      
@@ -64,7 +75,7 @@ public class SJFP {
      System.out.println("Process\tBurst time\tArrival time\tWaiting time\tTurnaround time ");
      for(int i = 1; i <= n; i++)
      {
-      System.out.printf("%d\t%2d\t\t%2d\t\t%2d\t\t%2d",i,process[i][1],process[i][0],process[i][2],process[i][3]);
+      System.out.printf("%d\t%2d\t\t%2d\t\t%2d\t\t%2d",i,process[i][4],process[i][0],process[i][2],process[i][3]);
       System.out.println();
      }
      
@@ -79,6 +90,16 @@ public class SJFP {
      }
      System.out.println("The Average Waiting Time is:"+WT/n);
      System.out.println("The Average Turnaround Time is:"+TT/n );
+     double cpuAfter = osMBean.getProcessCpuTime();
+     double nanoAfter = System.nanoTime();
+
+     double percent;
+     if (nanoAfter > nanoBefore)
+      percent = ((cpuAfter-cpuBefore)*100L)/
+        (nanoAfter-nanoBefore);
+     else percent = 0;
+
+     System.out.println("Cpu usage: "+percent+"%");
  }
     
 }
